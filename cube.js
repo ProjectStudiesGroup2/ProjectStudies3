7831e12ef5d2db2d364510b7b9fdb56a3d5ad252
BenjaminCollet <benjacollet@gmail.com>
2016-11-15 16:16:28 +0200

    added nice control for cube

--- cube.js
+++ cube.js
@@ -7,3 +7,65 @@ cube.castShadow = true;
 cube2.castShadow = true;
 scene.add(cube);
 scene.add(cube2);
+
+
+var speed = 20;
+var curr = cube;
+
+var pressed = {};
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case "a":
+            curr = cube;
+            break;
+        case "e":
+            curr = cube2;
+            break;
+    }
+
+    lv = curr.getLinearVelocity();
+
+    if (!pressed[event.key]) {
+        switch (event.key) {
+            case "z":
+                curr.setLinearVelocity(
+                    lv.add({ x: 0, y: 0, z: -speed })
+                );
+                break;
+            case "q":
+                curr.setLinearVelocity(
+                    lv.add({ x: -speed, y: 0, z: 0 })
+                );
+                break;
+            case "s":
+                curr.setLinearVelocity(
+                    lv.add({ x: 0, y: 0, z: speed })
+                );
+                break;
+            case "d":
+                curr.setLinearVelocity(
+                    lv.add({ x: speed, y: 0, z: 0 })
+                );
+                break;
+        }
+        pressed[event.key] = true;
+    }
+}, false);
+
+document.addEventListener('keyup', (event) => {
+    lv = curr.getLinearVelocity();
+
+    pressed[event.key] = false;
+    switch (event.key) {
+        case "z": case "s":
+            curr.setLinearVelocity(
+                lv.add({ x: 0, y: 0, z: -lv.z })
+            );
+            break;
+        case "q": case "d":
+            curr.setLinearVelocity(
+                lv.add({ x: -lv.x, y: 0, z: 0 })
+            );
+            break;
+    }
+}, false);
