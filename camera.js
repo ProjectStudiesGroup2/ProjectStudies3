7d223f0dbca7da81a9dbbc865f781b522725a897
BenjaminCollet <benjacollet@gmail.com>
2016-11-15 11:00:57 +0200

    improvement of the js architecture

---
+++ camera.js
@@ -0,0 +1,3 @@
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+camera.position.z = 5;
--- cube.js
+++ cube.js
@@ -1,6 +1,4 @@
-function addCube() {
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new Physijs.BoxMesh(geometry, material);
-    scene.add(cube);
-}
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new Physijs.BoxMesh(geometry, material);
+scene.add(cube);
