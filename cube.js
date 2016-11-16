df5fd03853eb76cbbc9c2cd879ea391bd317a78c
BenjaminCollet <benjacollet@gmail.com>
2016-11-16 10:36:46 +0200

    'clean' merge

--- cube.js
+++ cube.js
@@ -1,54 +1,71 @@
-var cube1 = new Physijs.BoxMesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshLambertMaterial({ color: 0x1c5400 })
-);
-cube1.position.set(0, 10, 0);
-cube1.castShadow = true;
-scene.add(cube1);
-
-var cube2 = new Physijs.BoxMesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshLambertMaterial({ color: 0x1c5400 })
-);
-cube2.castShadow = true;
-scene.add(cube2);
+var cubes = {};
+for (var i = 0; i < 2; i++) {
+    cubes[i] = new Physijs.BoxMesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshLambertMaterial({ color: 0x805900 })
+    );
+    cubes[i].position.set(i * 2 - 1, 0, 0);
+    cubes[i].castShadow = true;
+    scene.add(cubes[i]);
+}
 
 
 
-var speed = 20;
-var curr = cube1;
-var cubes = [cube1, cube2]; var idx = 0;
-curr.material.color.set(0x38a800);
+// function getTheClosestTo(vec) {
+//     var min = Infinity; var res;
+//
+//     for (var i in cubes) {
+//         if (cubes[i].position.distanceTo(vec) < min && i != current)
+//             res = i;
+//     }
+//
+//     return res;
+// }
+//
+//
+//
+// function changeCurrent() {
+//     cubes[current].material.color.set(0x805900);
+//
+//     current = getTheClosestTo({ x: 0, y: 0, z: 0 });;
+//
+//     cubes[current].material.color.set(0xbf8600);
+// }
 
-var pressed = {};
+
+var current = 0;
+cubes[current].material.color.set(0xbf8600);
+// changeCurrent();
+
+var speed = 20; var pressed = {};
 document.addEventListener('keydown', (event) => {
     if (event.code == "KeyE") {
-        curr.material.color.set(0x1c5400);
-        idx += 1; idx %= 2; curr = cubes[idx];
-        curr.material.color.set(0x38a800);
+        cubes[current].material.color.set(0x805900);
+        current += 1; current %= 2;
+        cubes[current].material.color.set(0xbf8600);
     }
 
-    lv = curr.getLinearVelocity();
+    lv = cubes[current].getLinearVelocity();
 
     if (!pressed[event.code]) {
         switch (event.code) {
             case "KeyW":
-                curr.setLinearVelocity(
+                cubes[current].setLinearVelocity(
                     lv.add({ x: 0, y: 0, z: -speed })
                 );
                 break;
             case "KeyA":
-                curr.setLinearVelocity(
+                cubes[current].setLinearVelocity(
                     lv.add({ x: -speed, y: 0, z: 0 })
                 );
                 break;
             case "KeyS":
-                curr.setLinearVelocity(
+                cubes[current].setLinearVelocity(
                     lv.add({ x: 0, y: 0, z: speed })
                 );
                 break;
             case "KeyD":
-                curr.setLinearVelocity(
+                cubes[current].setLinearVelocity(
                     lv.add({ x: speed, y: 0, z: 0 })
                 );
                 break;
@@ -58,17 +75,17 @@ document.addEventListener('keydown', (event) => {
 }, false);
 
 document.addEventListener('keyup', (event) => {
-    lv = curr.getLinearVelocity();
+    lv = cubes[current].getLinearVelocity();
 
     pressed[event.code] = false;
     switch (event.code) {
         case "KeyW": case "KeyS":
-            curr.setLinearVelocity(
+            cubes[current].setLinearVelocity(
                 lv.add({ x: 0, y: 0, z: -lv.z })
             );
             break;
         case "KeyA": case "KeyD":
-            curr.setLinearVelocity(
+            cubes[current].setLinearVelocity(
                 lv.add({ x: -lv.x, y: 0, z: 0 })
             );
             break;
