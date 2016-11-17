7052ee6fb17b9d2b5a1f4f07ae9a5b8ad8ab11ac
HYDROGEN\Dimitrios <mrhydro7k@gmail.com>
2016-11-17 22:54:50 +0200

    Added ball reposition after going out of bounds

--- render.js
+++ render.js
@@ -6,10 +6,44 @@ var render = function() {
     if (gamepads[0]) {
         cubes.useGamepad(gamepads[0]);
     }
-    detectCollision();    
-    
+    detectCollision();
+
+      //*** ball reset ***//
+    if (ball.position.x <= -fieldWidth/2){
+      ball.position.set(0, 4, 0);
+      ball.__dirtyPosition = true;
+      ball.rotation.set(0, 0, 0);
+      ball.__dirtyRotation = true;
+      ball.setLinearVelocity({ x: 0, y: 0, z: 0 });
+      ball.setAngularVelocity({ x: 0, y: 0, z: 0 });
+      }
+    if (ball.position.x >= fieldWidth/2){
+      ball.position.set(0, 4, 0);
+      ball.__dirtyPosition = true;
+      ball.rotation.set(0, 0, 0);
+      ball.__dirtyRotation = true;
+      ball.setLinearVelocity({ x: 0, y: 0, z: 0 });
+      ball.setAngularVelocity({ x: 0, y: 0, z: 0 });
+    }
+    if (ball.position.z <= -fieldHeight/2){
+      ball.position.set(0, 4, 0);
+      ball.__dirtyPosition = true;
+      ball.rotation.set(0, 0, 0);
+      ball.__dirtyRotation = true;
+      ball.setLinearVelocity({ x: 0, y: 0, z: 0 });
+      ball.setAngularVelocity({ x: 0, y: 0, z: 0 });
+    }
+    if (ball.position.z >= fieldHeight/2){
+      ball.position.set(0, 4, 0);
+      ball.__dirtyPosition = true;
+      ball.rotation.set(0, 0, 0);
+      ball.__dirtyRotation = true;
+      ball.setLinearVelocity({ x: 0, y: 0, z: 0 });
+      ball.setAngularVelocity({ x: 0, y: 0, z: 0 });
+    }
+
     if (collizionDet == true) {
-        ball.position.set( cubes.player.position.x, cubes.player.position.y + 0.9, cubes.player.position.z - 2);        
+        ball.position.set( cubes.player.position.x, cubes.player.position.y + 0.9, cubes.player.position.z - 2);
         ball.__dirtyPosition = true;
     }
 
