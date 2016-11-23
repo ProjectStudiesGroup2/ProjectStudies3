fff6a348abbf2e2c8698b12e6ab73639e18a4404
Sofia Starikova <t4stso01@students.oamk.fi>
2016-11-23 12:32:35 +0200

    add goalie

---
+++ goalie.js
@@ -0,0 +1,19 @@
+
+    /*** Object goalies for both commands ***/
+var goalie1 = new Physijs.BoxMesh(
+   new THREE.BoxGeometry(1, 1, 1),
+   new THREE.MeshLambertMaterial({ color: 0xffffff})
+);
+goalie1.position.set(0, 5.5, -80);
+
+goalie1.castShadow = true;
+scene.add(goalie1);
+
+var goalie2 = new Physijs.BoxMesh(
+   new THREE.BoxGeometry(1, 1, 1),
+   new THREE.MeshLambertMaterial({ color: 0xffffff})
+);
+goalie2.position.set(0, 5.5, 80);
+
+goalie2.castShadow = true;
+scene.add(goalie2);
---
+++ zoomcam.js
@@ -0,0 +1,11 @@
+var arrow = new THREE.ArrowHelper(
+			new THREE.Vector3( 0.5, 0.5, 0.5 ),
+			new THREE.Vector3( 0, 2, 20 ),
+			15,
+			0xff3300
+		);
+
+arrow.position.set(0, 5.5, -80);
+		scene.add(arrow);
+
+
